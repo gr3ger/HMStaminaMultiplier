15cd39b0c34830a9277d626aec7602d4512062a6
gr3ger <gr3ger@gmail.com>
2025-07-29 21:05:01 +0200

    Initial commit

---
+++ HarvestMoonML/HMStaminaMultiplier.cs
@@ -0,0 +1,41 @@
+﻿using HarmonyLib;
+using MelonLoader;
+using player;
+using Table;
+
+[assembly: MelonInfo(typeof(HarvestMoonML.HMStaminaMultiplier), "HM:WoA stamina mult", "1.0.0", "Greger", null)]
+[assembly: MelonGame("Natsume Inc.", "Harvest Moon: The Winds of Anthos")]
+
+namespace HarvestMoonML
+{
+    public class HMStaminaMultiplier : MelonMod
+    {
+        private MelonPreferences_Category mainPrefCategory;
+        private MelonPreferences_Entry<float> prefMultiplier;
+        
+        public override void OnInitializeMelon()
+        {
+            mainPrefCategory = MelonPreferences.CreateCategory("HMStaminaMultiplier");
+            prefMultiplier = mainPrefCategory.CreateEntry("Stamina damage multiplier", 0.5f);
+            mainPrefCategory.SaveToFile();
+            
+            LoggerInstance.Msg($"Initialized with a stamina damage multiplier of {prefMultiplier.Value}");
+        }
+        
+        [HarmonyPatch(typeof(HpCostController), "ApplyHpCostImpl")]
+        public static class ApplyHpCostImplNew {
+            [HarmonyPrefix]
+            public static bool Prefix(PLAYERHPCOST.Defs id)
+            {
+                var orgDamage = TableAccessor.PlayerHpCost.get_param((int)id);
+                var newDamage = (int)Math.Floor(orgDamage * Melon<HMStaminaMultiplier>.Instance.prefMultiplier.Value);
+                //Melon<HMStaminaMultiplier>.Logger.Msg($"Taking {newDamage} damage. Originally {orgDamage} damage.");
+                SingletonMonoBehaviour<GameInfo>.Instance.PlayerInfo.Hp -= newDamage;
+                //Melon<HMStaminaMultiplier>.Logger.Msg($"Current health after changes: {SingletonMonoBehaviour<GameInfo>.Instance.PlayerInfo.Hp}");
+                
+                // Returning false so we skip running the original function
+                return false;
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+﻿## Harvest Moon: The Winds of Anthos
+A MelonLoader mod to adjust stamina consumption in the game.
+
+### Installation
+1. Install [MelonLoader](https://github.com/LavaGang/MelonLoader) (0.7.1 was used when developing the mod)
+2. Download the DLL from [Releases](https://github.com/gr3ger/HMStaminaMultiplier/releases/latest) and drop it in the `Mods` folder
+3. Start the game
+4. Now a configuration file has been created (`UserData/MelonPreferences.cfg`) where you can configure what multiplier you want to use. By default it multiplies the original damage by 0.5, which effectively gives you 2x stamina.
